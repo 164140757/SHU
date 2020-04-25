ef2400a8d5d048560b7b7b66401415bad1df4e21
WhiteDream <43661768+164140757@users.noreply.github.com>
2020-04-25 12:45:25 +0800

    update

---
+++ Openmp/cmake-build-debug/CMakeFiles/Openmp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Openmp.dir/main.cpp.obj"
+  "Openmp.exe"
+  "Openmp.exe.manifest"
+  "Openmp.pdb"
+  "libOpenmp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Openmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
